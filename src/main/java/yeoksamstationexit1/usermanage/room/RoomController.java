bac645db142aa8bb0777129cf872a15defdaaef2
shimshim <11goatmxj@naver.com>
2023-11-08 12:59:44 +0900

    api수정

--- src/main/java/yeoksamstationexit1/usermanage/room/RoomController.java
+++ src/main/java/yeoksamstationexit1/usermanage/room/RoomController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
-@RequestMapping("api/room")
+@RequestMapping("/user-manage/user")
 @RestController
 @Tag(name = "약속방", description = "약속방 관련 API 입니다.")
 public class RoomController {
